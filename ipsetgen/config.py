70a6f673646e43a09d2f14fc903ad52b46eb553b
Jon Chen <bsd@voltaire.sh>
2014-11-01 18:31:10 -0400

    stub in Config object

--- ipsetgen/config.py
+++ ipsetgen/config.py
@@ -2,10 +2,19 @@ import os
 import yaml
 
 
+_DEFAULT_CONFIG_PATH = '/etc/ipsetgen/'
+
+
 def _load_all_files(self):
     for config_file in os.listdir(self.config_path):
         with open(config_file, 'r') as fh:
             yield yaml.load(fh)
+
+
+class Config(object):
+    def __init__(self, config_path=_DEFAULT_CONFIG_PATH):
+        self.config_path = config_path
+        self.configs = _load_all_files(self.config_path)
 
 
 class Service(object):
