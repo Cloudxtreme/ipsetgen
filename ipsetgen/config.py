9c555452505b4a805e16f7b156783ba55a97de45
Jon Chen <bsd@voltaire.sh>
2014-11-01 18:31:37 -0400

    _load_all_files does not have self

--- ipsetgen/config.py
+++ ipsetgen/config.py
@@ -5,8 +5,8 @@ import yaml
 _DEFAULT_CONFIG_PATH = '/etc/ipsetgen/'
 
 
-def _load_all_files(self):
-    for config_file in os.listdir(self.config_path):
+def _load_all_files(path):
+    for config_file in os.listdir(path):
         with open(config_file, 'r') as fh:
             yield yaml.load(fh)
 
