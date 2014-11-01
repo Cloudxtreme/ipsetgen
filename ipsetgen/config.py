900b05d4be3984bb6da1b7aafda4c0d15d82252f
Jon Chen <bsd@voltaire.sh>
2014-11-01 18:20:57 -0400

    stub out objects

--- ipsetgen/config.py
+++ ipsetgen/config.py
@@ -6,3 +6,25 @@ def _load_all_files(self):
     for config_file in os.listdir(self.config_path):
         with open(config_file, 'r') as fh:
             yield yaml.load(fh)
+
+
+class Service(object):
+    def __init__(self, service, ports=None):
+        self.service = service
+        self.ports = ports
+
+class Port(object):
+    def __init__(self, port, rules):
+        self.port = port
+        self.rules = rules
+
+class Rule(object):
+    def __init__(self, rule, roles):
+        self.rule = rule
+        self.roles = roles
+
+class Role(object):
+    def __init__(self, name, ipv4_addrs=[], ipv6_addrs=[]):
+        self.name = name
+        self.ipv4_addrs = ipv4_addrs
+        self.ipv6_addrs = ipv6_addrs
