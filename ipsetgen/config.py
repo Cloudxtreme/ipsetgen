9465b68346365dd08db99d1ae42e88de4306b8f2
Jon Chen <bsd@voltaire.sh>
2014-11-01 18:13:18 -0400

    _load_all_files is an orphaned helper function

--- ipsetgen/config.py
+++ ipsetgen/config.py
@@ -2,8 +2,7 @@ import os
 import yaml
 
 
-class Config(object):
-    def _load_all_files(self):
-        for config_file in os.listdir(self.config_path):
-            with open(config_file, 'r') as fh:
-                yield yaml.load(fh)
+def _load_all_files(self):
+    for config_file in os.listdir(self.config_path):
+        with open(config_file, 'r') as fh:
+            yield yaml.load(fh)
