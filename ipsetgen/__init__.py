80c1427227c5c971bfbe0a850666764a3dd979f9
Jon Chen <bsd@voltaire.sh>
2014-11-02 12:44:18 -0500

    init generate_set

--- ipsetgen/__init__.py
+++ ipsetgen/__init__.py
@@ -8,4 +8,9 @@ class IPSet(object):
             self.ipset_cmd = 'ipset'
         else:
             self.ipset_cmd = ipset_cmd
-        _ = subprocess.check_output([ipset_cmd, '-v'])
+        _ = subprocess.check_output((ipset_cmd, '-v'))
+
+    def generate_set(self, set_name):
+        _destroy = (self.ipset_cmd, 'destroy', set_name)
+
+        _ = subprocess.check_output(_destroy)
