9ef7a8a69dc18241f6fda6113d7c6fe0d4266d19
Jon Chen <bsd@voltaire.sh>
2014-11-03 00:32:48 -0500

    need to specify path to config_file

--- ipsetgen/yaml.py
+++ ipsetgen/yaml.py
@@ -5,16 +5,16 @@ import yaml
 _DEFAULT_CONFIG_PATH = '/etc/ipsetgen/'
 
 
-def _load_all_files(path):
-    for config_file in os.listdir(path):
-        with open(config_file, 'r') as fh:
-            yield yaml.load(fh)
-
-
 class Config(object):
     def __init__(self, config_path=_DEFAULT_CONFIG_PATH):
         self.config_path = config_path
-        self.configs = _load_all_files(self.config_path)
+        self.configs = self._load_all_files()
+
+    def _load_all_files(self):
+        for config_file in os.listdir(self.config_path):
+            if os.path.isfile(self.config_path + config_file)
+            with open(self.config_path + config_file, 'r') as fh:
+                yield yaml.load(fh)
 
 
 class Port(object):
