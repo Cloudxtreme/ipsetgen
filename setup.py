a29c268c32a297f1d1a9fe62483d6c61301918eb
Jon Chen <bsd@voltaire.sh>
2014-11-04 20:08:02 -0500

    bump to v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='ipsetgen',
-    version='v0.0.1',
+    version='v0.0.2',
     packages=['ipsetgen'],
     include_package_data=True,
     license='BSD 3-Clause License',
